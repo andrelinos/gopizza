573f6f8b22ed2bee9b099805e914c589ebc9eebc
Andrelino Silva <andrelinocom@gmail.com>
2022-01-22 15:29:31 -0300

    Add: TypeScript, Paths and mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'react-native-reanimated/plugin',
+            [
+                'module-resolver',
+                {
+                    root: ['./src'],
+                    extensions: ['.ts', '.tsx', '.js', '.json'],
+                    alias: {
+                        '@components': './src/components',
+                        '@screens': './src/screens',
+                        '@assets': './src/assets',
+                        '@hooks': './src/hooks',
+                        '@services': './src/services',
+                        '@styles': './src/styles'
+                    }
+                }
+            ]
+        ]
+    };
 };
